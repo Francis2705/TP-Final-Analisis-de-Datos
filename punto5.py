95690abeffc66e5262600536ecfdd8497c24a1a0
agus2023 <landeira2017@gmail.com>
2025-07-07 19:13:12 -0300

    aplicando inflacion

--- punto5.py
+++ punto5.py
@@ -64,14 +64,33 @@ plt.show()
 
 """
 
+#definimos los factores IPC para ajustar a valores constantes 2024
+factores_ipc_2024 = {
+    2016: 53.93,
+    2017: 40.07,
+    2018: 32.10,
+    2019: 21.73,
+    2020: 14.12,
+    2021: 10.38,
+    2022: 6.88,
+    2023: 3.18,
+    2024: 1.00,
+}
+
 df = pd.read_csv("datos_filtrados_amba.txt", sep=';')
 
 # Convertir columna a numérica si hay valores no válidos
 df["P47T"] = pd.to_numeric(df["P47T"], errors="coerce")
 
-# Agrupar por aglomerado y calcular ingreso promedio
-datos_eph = df.groupby("AGLOMERADO", as_index=False)["P47T"].mean()
-datos_eph.rename(columns={"P47T": "ingreso_promedio"}, inplace=True)
+#Creamos nueva columna con el factor según el año de cada fila
+df["factor_ipc"] = df["ANO4"].map(factores_ipc_2024)
+
+#Calculamos el ingreso ajustado por inflación
+df["P47T_ajustado"] = df["P47T"] * df["factor_ipc"]
+
+# agrupamos por aglomerado y calcular ingreso promedio ajustado
+datos_eph = df.groupby("AGLOMERADO", as_index=False)["P47T_ajustado"].mean()
+datos_eph.rename(columns={"P47T_ajustado": "ingreso_promedio_ajustado"}, inplace=True)
 
 print(datos_eph)
 
@@ -79,23 +98,20 @@ print(datos_eph)
 # Hacer el merge:Operacion para juntar dos tablas,es decir,dos dataframes
 # left_on: columna del DataFrame izquierdo que se usará para emparejar
 # right_on: columna del DataFrame derecho para emparejar
+# Merge con buenos_aires
 datos_enriquecidos = buenos_aires.merge(datos_eph, left_on="eph_codagl", right_on="AGLOMERADO")
-
-
-# Reproyectar los datos enriquecidos
 datos_enriquecidos = datos_enriquecidos.to_crs(epsg=3857)
 
-# Graficar usando ingreso promedio
-ax = datos_enriquecidos.plot(column='ingreso_promedio',
-                                cmap='YlOrBr',  # Colores de calor
+# Mapa usando ingreso ajustado
+ax = datos_enriquecidos.plot(column='ingreso_promedio_ajustado',
+                                cmap='YlOrBr',
                                 legend=True,
                                 figsize=(10, 10),
                                 edgecolor='black',
                                 alpha=0.7)
 
-# Agregar mapa base
 ctx.add_basemap(ax, crs=datos_enriquecidos.crs.to_string(), source=ctx.providers.OpenStreetMap.Mapnik)
 
-plt.title("Ingreso promedio por aglomerado (EPH)")
+plt.title("Ingreso promedio ajustado a precios 2024 por aglomerado (EPH)")
 plt.axis('off')
 plt.show()
